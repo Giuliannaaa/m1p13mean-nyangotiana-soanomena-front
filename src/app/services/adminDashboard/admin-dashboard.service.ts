a73cc0246773bc94fd7903ef036b0af61080551c
Ny Angotiana <ralambo.nyango@gmail.com>
2026-02-17 21:09:37 +0300

    Init admin dashboard (income statistics, rank of the best store, number of store per category)

--- src/app/services/adminDashboard/admin-dashboard.service.ts
+++ src/app/services/adminDashboard/admin-dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environnements/environnement';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AdminDashboard } from '../../models/adminDashboar.model';
 
@@ -12,8 +12,39 @@ export class AdminDashboardService {
 
   constructor(private http: HttpClient) { }
 
-  getAdminDashboardData(): Observable<AdminDashboard> {
-    return this.http.get<AdminDashboard>(`${this.apiUrl}/data`);
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('auth_token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
   }
 
+  getAdminDashboardData(): Observable<AdminDashboard> {
+    return this.http.get<AdminDashboard>(`${this.apiUrl}/data`, { headers: this.getHeaders() });
+  }
+
+  // for Admin
+  getRevenuePerStore(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/revenue-per-store`, { headers: this.getHeaders() });
+  }
+
+  // for Admin
+  getStoreCountByCategory(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/store-count-by-category`, { headers: this.getHeaders() });
+  }
+
+  // for store owner
+  getStoreRevenue(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/my-store-revenue`, { headers: this.getHeaders() });
+  }
+
+  // for admin
+  getTop5Stores(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/top-5-stores`, { headers: this.getHeaders() });
+  }
+
+  // for store owner
+  getNumberOfProductInMyStore(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/number-of-product-in-my-store`, { headers: this.getHeaders() });
+  }
 }
